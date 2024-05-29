07fcaf2c55330b69ebd3034f1f2552e3138fd04c
Silva-6 <silva6media@gmail.com>
2024-05-29 23:33:44 +0100

    Define the neural network structure

--- 0-neural_networks-nd-deep_learning/2-Planar_data_classification_with_one_hidden_layer/0-layer_sizes.py
+++ 0-neural_networks-nd-deep_learning/2-Planar_data_classification_with_one_hidden_layer/0-layer_sizes.py
@@ -22,3 +22,12 @@ def layer_sizes(X, Y):
     n_y = Y.shape[0]
     # YOUR CODE ENDS HERE
     return n_x, n_h, n_y
+
+
+t_X, t_Y = layer_sizes_test_case()
+(n_x, n_h, n_y) = layer_sizes(t_X, t_Y)
+print("The size of the input layer is: n_x = " + str(n_x))
+print("The size of the hidden layer is: n_h = " + str(n_h))
+print("The size of the output layer is: n_y = " + str(n_y))
+
+layer_sizes_test(layer_sizes)
