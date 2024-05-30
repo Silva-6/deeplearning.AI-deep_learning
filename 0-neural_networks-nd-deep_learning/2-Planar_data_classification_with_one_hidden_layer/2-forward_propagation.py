fbcc083f88b631d567e8f7373c45c0099218b433
Silva-6 <silva6media@gmail.com>
2024-05-30 23:56:14 +0100

    implement forward propagation

--- 0-neural_networks-nd-deep_learning/2-Planar_data_classification_with_one_hidden_layer/2-forward_propagation.py
+++ 0-neural_networks-nd-deep_learning/2-Planar_data_classification_with_one_hidden_layer/2-forward_propagation.py
@@ -44,3 +44,10 @@ def forward_propagation(X, parameters):
              "A2": A2}
 
     return A2, cache
+
+
+t_X, parameters = forward_propagation_test_case()
+A2, cache = forward_propagation(t_X, parameters)
+print("A2 = " + str(A2))
+
+forward_propagation_test(forward_propagation)
