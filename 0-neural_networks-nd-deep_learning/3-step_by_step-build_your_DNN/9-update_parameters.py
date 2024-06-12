de16d027cd470759fdb29f5f77d2a992207e3af0
Silva-6 <silva6media@gmail.com>
2024-06-12 22:42:19 +0100

    implement update_parameters() using gradient descent - Congratulations!!

---
+++ 0-neural_networks-nd-deep_learning/3-step_by_step-build_your_DNN/9-update_parameters.py
@@ -0,0 +1,39 @@
+# GRADED FUNCTION: update_parameters
+
+def update_parameters(params, grads, learning_rate):
+    """
+    Update parameters using gradient descent
+
+    Arguments:
+    params -- python dictionary containing your parameters
+    grads -- python dictionary containing your gradients, output of L_model_backward
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+                  parameters["W" + str(l)] = ...
+                  parameters["b" + str(l)] = ...
+    """
+    parameters = copy.deepcopy(params)
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    # Update rule for each parameter. Use a for loop.
+    # (≈ 2 lines of code)
+    for l in range(L):
+        # parameters["W" + str(l+1)] = ...
+        # parameters["b" + str(l+1)] = ...
+        # YOUR CODE STARTS HERE
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+        # YOUR CODE ENDS HERE
+    return parameters
+
+
+t_parameters, grads = update_parameters_test_case()
+t_parameters = update_parameters(t_parameters, grads, 0.1)
+
+print ("W1 = "+ str(t_parameters["W1"]))
+print ("b1 = "+ str(t_parameters["b1"]))
+print ("W2 = "+ str(t_parameters["W2"]))
+print ("b2 = "+ str(t_parameters["b2"]))
+
+update_parameters_test(update_parameters)
