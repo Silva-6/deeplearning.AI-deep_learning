4db5f7167706d07b123376610d708f1a573a464f
Silva-6 <silva6media@gmail.com>
2024-06-19 16:36:12 +0100

    optimization method assignment

---
+++ 1-improving_deep_neural_networks/3-optimization_methods/update_parameters_with_gd.py
@@ -0,0 +1,30 @@
+# GRADED FUNCTION: update_parameters_with_gd
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    """
+    Update parameters using one step of gradient descent
+
+    Arguments:
+    parameters -- python dictionary containing your parameters to be updated:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients to update each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    learning_rate -- the learning rate, scalar.
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    """
+    L = len(parameters) // 2  # number of layers in the neural networks
+
+    # Update rule for each parameter
+    for l in range(1, L + 1):
+        # (approx. 2 lines)
+        # parameters["W" + str(l)] =
+        # parameters["b" + str(l)] =
+        # YOUR CODE STARTS HERE
+        parameters["W" + str(l)] = parameters["W" + str(l)] - learning_rate * grads["dW" + str(l)]
+        parameters["b" + str(l)] = parameters["b" + str(l)] - learning_rate * grads["db" + str(l)]
+        # YOUR CODE ENDS HERE
+    return parameters
