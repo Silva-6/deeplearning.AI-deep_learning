dd18b25fcdaf33d09009a253018778b02810cbd5
Silva-6 <silva6media@gmail.com>
2024-06-20 21:54:30 +0100

    implement the parameters update with momentum

---
+++ 1-improving_deep_neural_networks/3-optimization_methods/3-update_parameters_with_momentum.py
@@ -0,0 +1,58 @@
+# GRADED FUNCTION: update_parameters_with_momentum
+
+def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
+    """
+    Update parameters using Momentum
+
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- python dictionary containing the current velocity:
+                    v['dW' + str(l)] = ...
+                    v['db' + str(l)] = ...
+    beta -- the momentum hyperparameter, scalar
+    learning_rate -- the learning rate, scalar
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    v -- python dictionary containing your updated velocities
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+
+    # Momentum update for each parameter
+    for l in range(1, L + 1):
+        # (approx. 4 lines)
+        # compute velocities
+        # v["dW" + str(l)] = ...
+        # v["db" + str(l)] = ...
+        # update parameters
+        # parameters["W" + str(l)] = ...
+        # parameters["b" + str(l)] = ...
+        # YOUR CODE STARTS HERE
+        v["dW" + str(l)] = beta * v["dW" + str(l)] + (1 - beta) * grads['dW' + str(l)]
+        v["db" + str(l)] = beta * v["db" + str(l)] + (1 - beta) * grads['db' + str(l)]
+        parameters["W" + str(l)] = parameters["W" + str(l)] - learning_rate * v["dW" + str(l)]
+        parameters["b" + str(l)] = parameters["b" + str(l)] - learning_rate * v["db" + str(l)]
+        # YOUR CODE ENDS HERE
+
+    return parameters, v
+
+
+parameters, grads, v = update_parameters_with_momentum_test_case()
+
+parameters, v = update_parameters_with_momentum(parameters, grads, v, beta = 0.9, learning_rate = 0.01)
+print("W1 = \n" + str(parameters["W1"]))
+print("b1 = \n" + str(parameters["b1"]))
+print("W2 = \n" + str(parameters["W2"]))
+print("b2 = \n" + str(parameters["b2"]))
+print("v[\"dW1\"] = \n" + str(v["dW1"]))
+print("v[\"db1\"] = \n" + str(v["db1"]))
+print("v[\"dW2\"] = \n" + str(v["dW2"]))
+print("v[\"db2\"] = v" + str(v["db2"]))
+
+update_parameters_with_momentum_test(update_parameters_with_momentum)
