84be23fd45334cf7fe8b86c310c27ad52275f16c
Silva-6 <silva6media@gmail.com>
2024-06-20 21:49:19 +0100

    initialize velocity

---
+++ 1-improving_deep_neural_networks/3-optimization_methods/2-initialize_velocity.py
@@ -0,0 +1,43 @@
+# GRADED FUNCTION: initialize_velocity
+
+def initialize_velocity(parameters):
+    """
+    Initializes the velocity as a python dictionary with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL"
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+
+    Returns:
+    v -- python dictionary containing the current velocity.
+                    v['dW' + str(l)] = velocity of dWl
+                    v['db' + str(l)] = velocity of dbl
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v = {}
+
+    # Initialize velocity
+    for l in range(1, L + 1):
+        # (approx. 2 lines)
+        # v["dW" + str(l)] =
+        # v["db" + str(l)] =
+        # YOUR CODE STARTS HERE
+        v["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        v["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l + 1)])
+        # YOUR CODE ENDS HERE
+
+    return v
+
+
+parameters = initialize_velocity_test_case()
+
+v = initialize_velocity(parameters)
+print("v[\"dW1\"] =\n" + str(v["dW1"]))
+print("v[\"db1\"] =\n" + str(v["db1"]))
+print("v[\"dW2\"] =\n" + str(v["dW2"]))
+print("v[\"db2\"] =\n" + str(v["db2"]))
+
+initialize_velocity_test(initialize_velocity)
