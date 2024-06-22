a4318203c60f877076893e6297e39e9e4ffb9c6c
Silva-6 <silva6media@gmail.com>
2024-06-22 11:22:49 +0100

    implement the parameters update with Adam

--- 1-improving_deep_neural_networks/3-optimization_methods/5-update_parameters_with_adam.py
+++ 1-improving_deep_neural_networks/3-optimization_methods/5-update_parameters_with_adam.py
@@ -81,3 +81,19 @@ def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01,
         ### END CODE HERE ###
 
     return parameters, v, s, v_corrected, s_corrected
+
+
+parameters = initialize_adam_test_case()
+
+v, s = initialize_adam(parameters)
+print("v[\"dW1\"] = \n" + str(v["dW1"]))
+print("v[\"db1\"] = \n" + str(v["db1"]))
+print("v[\"dW2\"] = \n" + str(v["dW2"]))
+print("v[\"db2\"] = \n" + str(v["db2"]))
+print("s[\"dW1\"] = \n" + str(s["dW1"]))
+print("s[\"db1\"] = \n" + str(s["db1"]))
+print("s[\"dW2\"] = \n" + str(s["dW2"]))
+print("s[\"db2\"] = \n" + str(s["db2"]))
+
+initialize_adam_test(initialize_adam)
+
