d0205decbe258cc359dfa49943de6610ee95b0f6
Silva-6 <silva6media@gmail.com>
2024-06-23 12:36:48 +0100

    use exponential weight decay with fixed interval scheduling

---
+++ 1-improving_deep_neural_networks/3-optimization_methods/9-schedule_lr_decay.py
@@ -0,0 +1,36 @@
+# GRADED FUNCTION: schedule_lr_decay
+
+def schedule_lr_decay(learning_rate0, epoch_num, decay_rate, time_interval=1000):
+    """
+    Calculates updated the learning rate using exponential weight decay.
+
+    Arguments:
+    learning_rate0 -- Original learning rate. Scalar
+    epoch_num -- Epoch number. Integer.
+    decay_rate -- Decay rate. Scalar.
+    time_interval -- Number of epochs where you update the learning rate.
+
+    Returns:
+    learning_rate -- Updated learning rate. Scalar
+    """
+    # (approx. 1 lines)
+    # learning_rate = ...
+    # YOUR CODE STARTS HERE
+    learning_rate = 1 / (1 + decay_rate * (epoch_num / time_interval)) * learning_rate
+    # YOUR CODE ENDS HERE
+    return learning_rate
+
+
+learning_rate = 0.5
+print("Original learning rate: ", learning_rate)
+
+epoch_num_1 = 10
+epoch_num_2 = 100
+decay_rate = 0.3
+time_interval = 100
+learning_rate_1 = schedule_lr_decay(learning_rate, epoch_num_1, decay_rate, time_interval)
+learning_rate_2 = schedule_lr_decay(learning_rate, epoch_num_2, decay_rate, time_interval)
+print("Updated learning rate after {} epochs: ".format(epoch_num_1), learning_rate_1)
+print("Updated learning rate after {} epochs: ".format(epoch_num_2), learning_rate_2)
+
+schedule_lr_decay_test(schedule_lr_decay)
