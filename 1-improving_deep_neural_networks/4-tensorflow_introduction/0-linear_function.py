bb0cc8e299634ef5bdcc431cbdbae847728e171b
Silva-6 <silva6media@gmail.com>
2024-06-24 18:03:10 +0100

    test linear function

--- 1-improving_deep_neural_networks/4-tensorflow_introduction/0-linear_function.py
+++ 1-improving_deep_neural_networks/4-tensorflow_introduction/0-linear_function.py
@@ -29,3 +29,11 @@ def linear_function():
     Y = tf.add(tf.matmul(W, X), b)  # Y = W@X + b
     # YOUR CODE ENDS HERE
     return Y
+
+
+result = linear_function()
+print(result)
+
+assert type(result) == EagerTensor, "Use the TensorFlow API"
+assert np.allclose(result, [[-2.15657382], [ 2.95891446], [-1.08926781], [-0.84538042]]), "Error"
+print("\033[92mAll test passed")
