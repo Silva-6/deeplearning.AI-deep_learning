b9dc577aff073669ffa6766d15a515a588345805
Silva-6 <silva6media@gmail.com>
2024-06-26 19:59:36 +0100

    Initialize the Parameters

---
+++ 1-improving_deep_neural_networks/4-tensorflow_introduction/3-initialize_parameters.py
@@ -0,0 +1,41 @@
+# GRADED FUNCTION: initialize_parameters
+
+def initialize_parameters():
+    """
+    Initializes parameters to build a neural network with TensorFlow. The shapes are:
+                        W1 : [25, 12288]
+                        b1 : [25, 1]
+                        W2 : [12, 25]
+                        b2 : [12, 1]
+                        W3 : [6, 12]
+                        b3 : [6, 1]
+
+    Returns:
+    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
+    """
+
+    initializer = tf.keras.initializers.GlorotNormal(seed=1)
+    # (approx. 6 lines of code)
+    # W1 = ...
+    # b1 = ...
+    # W2 = ...
+    # b2 = ...
+    # W3 = ...
+    # b3 = ...
+    # YOUR CODE STARTS HERE
+    W1 = tf.Variable(initializer(shape=([25, 12288])))
+    b1 = tf.Variable(initializer(shape=([25, 1])))
+    W2 = tf.Variable(initializer(shape=([12, 25])))
+    b2 = tf.Variable(initializer(shape=([12, 1])))
+    W3 = tf.Variable(initializer(shape=([6, 12])))
+    b3 = tf.Variable(initializer(shape=([6, 1])))
+    # YOUR CODE ENDS HERE
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+
+    return parameters
